2332502a85ee2e11abc8d701548a3ac0db6e2f42
EngineeringAwesome <robomower9508@gmail.com>
2015-06-17 17:42:55 -0400

    Example OpMode to Stop when the touch sensor is pressed

--- ExampleDriveWTouch.java
+++ ExampleDriveWTouch.java
@@ -1,6 +1,7 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
  * An empty op mode serving as a template for custom OpModes
@@ -9,6 +10,7 @@ public class ExampleDriveWTouch extends OpMode {
 
     DcMotor leftMotor;
     DcMotor rightMotor;
+    TouchSensor touchSensor;
 
     /*
     * Constructor
@@ -23,7 +25,15 @@ public class ExampleDriveWTouch extends OpMode {
     */
     @Override
     public void start() {
+        //get references to the motors from the hardware map
+        leftMotor = hardwareMap.dcMotor.get("motor_1");
+        rightMotor = hardwareMap.dcMotor.get("motor_2");
 
+        //reverse the left motor
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        //get a reference to the touch sensor
+        touchSensor = hardwareMap.touchSensor.get("touch");
     }
 
 
@@ -33,6 +43,16 @@ public class ExampleDriveWTouch extends OpMode {
     */
     @Override
     public void loop() {
+
+        if(touchSensor.isPressed()) {
+            //Stop the motors if the touch sensor is pressed
+            leftMotor.setPower(0);
+            rightMotor.setPower(0);
+        } else {
+            //Keep driving if the touch sensor is not pressed
+            leftMotor.setPower(0.5);
+            rightMotor.setPower(0.5);
+        }
 
     }
 
