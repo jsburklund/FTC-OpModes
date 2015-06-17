5f6f6c91785d781024686124ad4b3596a98d0cd9
EngineeringAwesome <robomower9508@gmail.com>
2015-06-17 17:43:37 -0400

    Example to back up and turn for a random amount of time when the touch sensor is pressed

---
+++ ExampleDriveWTouchBackupTurnRandom.java
@@ -0,0 +1,110 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * An empty op mode serving as a template for custom OpModes
+ */
+public class ExampleDriveWTouchBackupTurnRandom extends OpMode {
+
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+    TouchSensor touchSensor;
+    ElapsedTime timer;
+
+    //Robot states
+    enum State{Drive, Backup, Turn}
+    State state;
+
+    //Time constants
+    double BACKUP_TIME = 1.0;
+    double turnTime;
+
+
+    /*
+    * Constructor
+    */
+    public ExampleDriveWTouchBackupTurnRandom() {
+
+    }
+
+    /*
+    * Code to run when the op mode is first enabled goes here
+    * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
+    */
+    @Override
+    public void start() {
+        //get references to the motors from the hardware map
+        leftMotor = hardwareMap.dcMotor.get("motor_1");
+        rightMotor = hardwareMap.dcMotor.get("motor_2");
+
+        //reverse the left motor
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        //get a reference to the touch sensor
+        touchSensor = hardwareMap.touchSensor.get("touch");
+
+        //set the state to driving
+        state = State.Drive;
+    }
+
+
+    /*
+    * This method will be called repeatedly in a loop
+    * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
+    */
+    @Override
+    public void loop() {
+        switch(state) {
+            case Drive:
+                //Set the motors to drive forward at 50% power
+                leftMotor.setPower(0.5);
+                rightMotor.setPower(0.5);
+
+                //Switch states if the touch sensor is pressed
+                if(touchSensor.isPressed()) {
+                    state = State.Backup;
+                    //reset the timer for backing up
+                    timer.reset();
+                }
+                break;
+            case Backup:
+                //Set the motors to drive backwards at 25% power
+                leftMotor.setPower(0.25);
+                rightMotor.setPower(0.25);
+
+                //switch states when it has backed up for a set amount of time
+                if(timer.time() >= BACKUP_TIME) {
+                    state = State.Turn;
+                    //Calculate a random time to turn between 0.5 seconds and 1.5 seconds
+                    turnTime = 0.5 + Math.random();
+                    //reset the timer for turning
+                    timer.reset();
+                }
+                break;
+            case Turn:
+                //Set the motors to turn the robot right at 25% power
+                leftMotor.setPower(0.25);
+                rightMotor.setPower(0.25);`
+
+                //switch states when it has turned for a set amount of time
+                if(timer.time() >= turnTime) {
+                    state = State.Drive;
+                }
+                break;
+        }
+        telemetry.addData("state", "Current State: " + state.name());
+        telemetry.addData("turnTime", "Turn Time: " + turnTime + " sec.");
+    }
+
+    /*
+    * Code to run when the op mode is first disabled goes here
+    * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#stop()
+    */
+    @Override
+    public void stop() {
+
+    }
+}
